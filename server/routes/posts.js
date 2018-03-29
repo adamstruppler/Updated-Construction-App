86a1db32b07ffcf053314f5fb77b52ab96d409af
adamstruppler <adamstruppler@gmail.com>
2018-03-28 23:01:50 -0600

    Projects is off

---
+++ server/routes/posts.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const Router = express.Router()
+const Post = require('../models/Post')
+
+Router.route('/')
+  .get((req, res) => {
+    Post.find((err, posts) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: 'SUCCESS', data: posts})
+      }
+    })
+  })
+  .post((req, res) => {
+    const post = new Post()
+    post.setPostData(req.body)
+    post.setDate()
+    post.save((err, savedPost) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: 'SUCCESS', data: savedPost})
+      }
+    })
+  })
+
+Router.route('/:postId')
+  .get((req, res) => {
+    const postId = req.params.postId
+    Post.findById({_id: postId}, (err, post) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: `Found: ${postId}`, data: post})
+      }
+    })
+  })
+  .put((req, res) => {
+    const postId = req.params.postId
+    Post.findById({_id: postId}, (err, post) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        post.setProductData(req.body)
+        post.save((err, updatedPost) => {
+          if (err) {
+            res.json({error: err})
+          } else {
+            res.json({msg: `Updated: ${postId}`, data: updatedPost})
+          }
+        })
+      }
+    })
+  })
+  .delete((req, res) => {
+    const deletePost = req.params.postId
+    Post.remove({_id: deletePost}, (err, post) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: `Deleted: ${deletedPost}`, data: post})
+      }
+    })
+  })
+
+module.exports = Router
--- server/server.js
+++ server/server.js
@@ -2,15 +2,35 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
+const adminRoute = require('./routes/admins')
+const developerRoute = require('./routes/developers')
+const projectsRoutes = require('./routes/project')
+const postRoute = require('./routes/posts')
+const passport = require('passport')
+const session = require('express-session')
 
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
 
-mongoose.connect('mongodb://localhost/__YOUR_DB__')
+mongoose.connect('mongodb://localhost/__UPDATED_SCONSTRUCTION_DB__')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
+
+app.use(session({
+  name: 'Updated-Construction-App',
+  secret: 'alakjalsfjklas',
+  secure: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+require('./passport/Strategies')(passport)
+require('./passport/Routes')(app, passport)
+app.use('/api/porjects', projectsRoutes)
+app.use('/api/posts', postRoute)
+app.use('/api/developers', developerRoute)
+app.use('/api/admins', adminRoute)
 
 const server = app.listen(port, () => console.log(`Running on port: ${port}`))
 
